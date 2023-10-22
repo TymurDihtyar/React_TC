34c2d098a6770d80b737ca60b27e1d610e90659a
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-10-22 18:02:21 +0300

    Hw2_1_tc

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,15 +1,18 @@
 import React, {FC} from 'react';
 import {IPosts} from "../interface/interface";
+import css from './Post.module.css'
 
 interface IProps {
-    item:IPosts
+    item: IPosts
+    click: (id:number) => Promise<void>
 }
 
-const Post:FC<IProps> = ({item}) => {
+const Post:FC<IProps> = ({item, click}) => {
     const {id, title} = item
     return (
-        <div>
-            {}
+        <div className={css.Post}>
+            <h3>{id}. {title.toUpperCase()}</h3>
+            <button className={css.but} onClick={()=>{click(id)}}>Details</button>
         </div>
     );
 };
--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -5,18 +5,25 @@ import {useEffect} from "react";
 import {IPosts} from "../interface/interface";
 import {postsService} from "../service/postsService";
 import {Post} from "../Post/Post";
+import css from './Posts.module.css'
 
 const Posts = () => {
     const [posts, setPosts] = useState<IPosts[]>([]);
-
+    const [detail, setDetail] = useState<IPosts>(null);
 
     useEffect(() => {
         postsService.getAll().then(({data}) => setPosts(data))
     }, []);
 
+    const click = async (id: number): Promise<void> => {
+        await postsService.getById(id).then(({data}) => setDetail(data))
+        console.log(detail);
+    }
+
     return (
-        <div>
-            {posts.map(item => <Post key={item.id} item={item}/>)}
+        <div className={css.allPosts}>
+            {posts.map(item => <Post key={item.id} item={item} click={click}/>)}
+
         </div>
     );
 };
