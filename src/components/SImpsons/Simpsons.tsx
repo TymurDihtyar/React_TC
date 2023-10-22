8328e6d411a8d3a6b803b6a6c7144d868e259ee8
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-10-22 14:02:32 +0300

    Hw1_1_tc

--- src/components/SImpsons/Simpsons.tsx
+++ src/components/SImpsons/Simpsons.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import {simpsons} from "../constants/items";
+import {Simpson} from "../Simpson/Simpson";
+import {ISimpson} from "../../Inyerfaces/SimpsonInterface";
+
 
 const Simpsons = () => {
     return (
         <div>
-
+            {simpsons.map((item: ISimpson, index: number) => <Simpson key={index} item={item}/>)}
         </div>
     );
 };
--- src/components/Simpson/Simpson.tsx
+++ src/components/Simpson/Simpson.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, {FC} from 'react';
+import {ISimpson} from "../../Inyerfaces/SimpsonInterface";
 
-const Simpson = () => {
+interface IProps {
+    item: ISimpson
+}
+
+const Simpson:FC<IProps> = ({item}) => {
+    const {name, surname, age, photo, info }= item;
     return (
         <div>
 
