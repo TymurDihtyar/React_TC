64a02226bbf2b123416431ff7ab178af19ea5699
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-10-22 16:24:14 +0300

    Hw1_2_tc

--- src/components/Servises/personServises.ts
+++ src/components/Servises/personServises.ts
@@ -6,7 +6,7 @@ import {IData, IPerson} from "../interfaces/interfacesPerson";
 
 const personServises = {
     getAll: (): IRes<IData> => axiosServises.get(urls.persons.base),
-    getById: (id: number): IRes<IPerson> => axiosServises.get(urls.persons.byId(id))
+    getById: (id: number): IRes<IData> => axiosServises.get(urls.persons.byId(id))
 }
 
 export {personServises}
