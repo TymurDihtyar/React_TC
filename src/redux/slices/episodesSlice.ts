7e4e142a389b6620fe7e61a1f977a7bd8050c456
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-11-20 17:18:31 +0200

    Hw5.2

--- src/redux/slices/episodesSlice.ts
+++ src/redux/slices/episodesSlice.ts
@@ -1,5 +1,31 @@
 import {createSlice} from "@reduxjs/toolkit";
+import {IEpiside} from "../../interfaces/episodeInterface";
+
+interface IState {
+    episodes: IEpiside[],
+    prevPage: string,
+    nextPage: string,
+    episodeName: string
+}
+
+const initialState: IState = {
+    episodes: [],
+    prevPage: null,
+    nextPage: null,
+    episodeName: null,
+}
 
 const episodesSlice = createSlice({
+    name: 'episodesSlice',
+    initialState,
+    reducers: {},
 
 })
+
+const {reducer:episodesReducer, actions} = episodesSlice
+
+const episodesAction = {
+    ...actions
+}
+
+export {episodesReducer, episodesAction}
--- src/types/reduxType.ts
+++ src/types/reduxType.ts
@@ -1,5 +1,6 @@
 import {store} from "../redux/store";
-import {useDispatch} from "react-redux";
 
 type RootState = ReturnType<typeof store.getState>
-type AppDispatch = typeof useDispatch<>()
+type AppDispatch = typeof store.dispatch
+
+export type {RootState, AppDispatch}
