c3c6495f3d7190d4742aa1ca53cb53abe44cdc06
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-11-21 11:48:54 +0200

    Hw6

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import ReactDOM from 'react-dom/client';
 
-import {App} from './App';
-
+import './index.css'
+import {RouterProvider} from "react-router-dom";
+import {router} from "./router";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App/>
+    <RouterProvider router={router}/>
 );
 
 
